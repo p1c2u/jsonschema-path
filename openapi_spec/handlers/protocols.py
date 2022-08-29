b52e6fbe01d2ad925e1fe4569a7aaf1eff23a23e
p1c2u <maciag.artur@gmail.com>
2022-08-29 18:59:48 +0100

    Protocol compatibility fix

--- openapi_spec/handlers/protocols.py
+++ openapi_spec/handlers/protocols.py
@@ -1,8 +1,19 @@
+from typing import TYPE_CHECKING
 from typing import Optional
-from typing import Protocol
-from typing import runtime_checkable
+
+if TYPE_CHECKING:
+    from typing_extensions import Protocol
+    from typing_extensions import runtime_checkable
+else:
+    try:
+        from typing import Protocol
+        from typing import runtime_checkable
+    except ImportError:
+        from typing_extensions import Protocol
+        from typing_extensions import runtime_checkable
 
 
 @runtime_checkable
 class SupportsRead(Protocol):
-    def read(self, amount: Optional[int] = 0) -> str: ...
+    def read(self, amount: Optional[int] = 0) -> str:
+        ...
