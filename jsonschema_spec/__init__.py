9b0437e5b360dedc56bcdd718fac2741eee3bc1f
p1c2u <maciag.artur@gmail.com>
2023-06-11 09:08:40 +0100

    Version 0.1.6

--- jsonschema_spec/__init__.py
+++ jsonschema_spec/__init__.py
@@ -3,7 +3,7 @@ from jsonschema_spec.paths import Spec
 
 __author__ = "Artur Maciag"
 __email__ = "maciag.artur@gmail.com"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __url__ = "https://github.com/p1c2u/jsonschema-spec"
 __license__ = "Apache-2.0"
 
