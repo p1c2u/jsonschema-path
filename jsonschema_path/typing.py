6a9004b3ec96aa276fe522a116212355a49748e3
p1c2u <maciag.artur@gmail.com>
2025-06-07 21:39:10 +0100

    Schema types

--- jsonschema_path/typing.py
+++ jsonschema_path/typing.py
@@ -1,5 +1,17 @@
 from typing import Any
 from typing import Mapping
 
+from pathable.types import LookupKey as SchemaKey
+from pathable.types import LookupNode as SchemaNode
+from pathable.types import LookupValue as SchemaValue
+
+__all__ = [
+    "ResolverHandlers",
+    "Schema",
+    "SchemaNode",
+    "SchemaKey",
+    "SchemaValue",
+]
+
 ResolverHandlers = Mapping[str, Any]
 Schema = Mapping[str, Any]
